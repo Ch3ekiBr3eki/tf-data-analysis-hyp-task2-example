36bd74036b272695b31bbcd97685dc83c1641aa3
Anton <113473409+Ch3ekiBr3eki@users.noreply.github.com>
2023-04-13 20:37:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,5 @@ from scipy.stats import ks_2samp
 chat_id = 323297403  # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, p_value = ks_2samp(x, y)
-
-    return p_value < 0.02
+    statistic, critical_values, significance_level = stats.anderson_ksamp([x, y])
+    return statistic > critical_values[2]
